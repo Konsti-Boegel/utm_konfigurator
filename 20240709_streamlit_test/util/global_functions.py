b03aa9f4dc79ad434b4ae75000861ac28da96db2
Konstantin <konstantinboegel.ai@gmail.com>
2024-07-10 13:40:50 +0200

    New parameter - Interesse

--- 20240709_streamlit_test/util/global_functions.py
+++ 20240709_streamlit_test/util/global_functions.py
@@ -27,6 +27,8 @@ def create(param_dict, opt_param_dict):
     if null_params:
         st.error(f'Fehlende Angaben: {", ".join(null_params)}')
     else:
+        remove_empty_values(param_dict)
+        remove_empty_values(opt_param_dict)
         utm_link = 'https://{url}?utm_source={channel}&utm_medium={budget}_{channel_group}&utm_content={format}_{absender}_{interest}_{ID}&utm_campaign={project}'.format(
             url=param_dict.get('URL', 'NA'),
             channel=param_dict.get('Kanal', 'NA'),
@@ -51,3 +53,7 @@ def generate_id(length=10):
     # Kombiniere die Buchstaben und Zahlen mit Wiederholung der Zahlen, um die Chancen auszugleichen
     characters = letters + digits * (len(letters) // len(digits))
     return ''.join(random.choice(characters) for _ in range(length))
+
+
+def remove_empty_values(dict):
+    return {key: value for key, value in dict.items() if value != ""}
